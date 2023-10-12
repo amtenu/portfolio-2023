8a079c40ed26281c60ab9d553e5ad7daa43ffd6e
amtenu <amannov21@gmail.com>
2023-10-12 11:09:57 -0600

    Menu Array

---
+++ src/components/navBar/config.js
@@ -0,0 +1,26 @@
+const navMenus=[
+    {
+        label:'HOME',
+        to:'/'
+    },
+    {
+        label:'ABOUT ME',
+        to:'/about'
+    },
+    {
+        label:'RESUME',
+        to:'/resume'
+    },
+    {
+        label:'SKILLS',
+        to:'/skills'
+    },
+    {
+        label:'CONTACT',
+        to:'/contact'
+    },
+    {
+        label:'PORTFOLIO',
+        to:'/portfolio'
+    }
+]
